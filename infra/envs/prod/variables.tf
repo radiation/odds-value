11b4f71464df490a81bb0169d19c331cfa6644ae
Bryan Choate <bryan.choate@gmail.com>
2025-12-28 20:46:52 -0500

    Add initial terraform setup

---
+++ infra/envs/prod/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-east1"
+}
+
+variable "artifact_repo_id" {
+  type    = string
+  default = "sports-ev"
+}
+
+variable "db_instance_name" {
+  type    = string
+  default = "odds-value-pg"
+}
+
+variable "db_name" {
+  type    = string
+  default = "odds_value"
+}
+
+variable "db_user" {
+  type    = string
+  default = "odds_value_app"
+}
